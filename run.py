477f32e475ac7a063a9379ba95f413471a20320a
TURuibo <ruibo.tu@gmail.com>
2019-05-30 17:30:15 +0200

    rm the construction of simulator codes

--- run.py
+++ run.py
@@ -1,16 +1,15 @@
-from source import simulator as sltr
+from source import sampling as spl
 
 if __name__ == '__main__':
     confounder = False
-    with open('models/bnm.pickle', 'rb') as f:
-        # The protocol version used is detected automatically, so we do not
-        # have to specify it.
-        model = sltr.pickle.load(f)
-    columns = sltr.get_columns()
+    model = spl.load_pgm()
+    vars = sltr.get_var_nms()
     nun_sample = 100
+    
     if confounder:
-        df_sim = sltr.random_sample(model, ['Confounder_DLS_C'] + columns, nun_sample)
+        df_sim = spl.random_sample(model, ['Confounder_DLS_C'] + vars, nun_sample)
     else:
-        df_sim = sltr.random_sample(model, columns, nun_sample)
+        df_sim = spl.random_sample(model, vars, nun_sample)
+
     df_sim.to_csv('result/df_sim_'+str(nun_sample)+'_ex_conf.csv', index=True, header=True)
     print("Congratulation!")
